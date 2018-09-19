5cf4c242b32482c2abf007ed57df1ebc2c1906e5
EC2 Default User <ec2-user@ip-172-31-9-164.us-east-2.compute.internal>
2018-09-19 16:36:53 +0000

    basic tic tac toe works

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,13 @@
 function makeMove(cellID){
-    // var cell = document.getElementById("b1r1c1");
     
-    cellID.style.backgroundImage = "url(https://mbtskoudsalg.com/images/red-cross-png-2.png)";
+    var player = sessionStorage.getItem("player");
+    if (player == "x" || player == null){
+        cellID.style.backgroundImage = "url(https://mbtskoudsalg.com/images/red-cross-png-2.png)";
+        sessionStorage.setItem("player", "o");
+    }
+    else if (player == "o"){
+        cellID.style.backgroundImage = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Blue_circle_for_diabetes.svg/1024px-Blue_circle_for_diabetes.svg.png)";
+        sessionStorage.setItem("player", "x");
+    }
+        
 }
