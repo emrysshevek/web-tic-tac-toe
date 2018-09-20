8540eafac7c51c69e2c7643d5a75d02138ee65f6
techimera <masteraile@gmail.com>
2018-09-20 05:08:15 +0000

    added some files

--- tic-tac-toe.js
+++
@@ -1,5 +0,0 @@
-function makeMove(cellID){
-    // var cell = document.getElementById("b1r1c1");
-    
-    cellID.style.backgroundImage = "url(https://mbtskoudsalg.com/images/red-cross-png-2.png)";
-}
